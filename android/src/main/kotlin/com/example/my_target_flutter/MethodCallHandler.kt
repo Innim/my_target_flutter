990ee9a785a8cb974da884537ef82afc779e4957
Maksim <69686849+ulezkin@users.noreply.github.com>
2022-05-30 15:22:03 +0300

    [my_target_flutter] added errors result.

--- android/src/main/kotlin/com/example/my_target_flutter/MethodCallHandler.kt
+++ android/src/main/kotlin/com/example/my_target_flutter/MethodCallHandler.kt
@@ -12,11 +12,15 @@ import java.text.SimpleDateFormat
 import java.util.*
 
 
-class MethodCallHandler(private val context: Context, private val adListenerChannel: BasicMessageChannel<String>) : MethodChannel.MethodCallHandler {
+class MethodCallHandler(
+    private val context: Context,
+    private val adListenerChannel: BasicMessageChannel<String>
+) : MethodChannel.MethodCallHandler {
     companion object {
         private const val INITIAL = "initial"
         private const val LOAD = "load"
         private const val SHOW = "show"
+        private const val errorCode = "my_target_flutter error:"
     }
 
     private lateinit var ad: InterstitialAd
@@ -26,12 +30,8 @@ class MethodCallHandler(private val context: Context, private val adListenerChan
         when (call.method) {
             INITIAL -> {
                 val data = InitialData.fromArgs(call)
-                if (data.slotId != null) {
-                    val res =
-                        initAd(context, data.slotId, data.useDebugMode ?: false, data.testDevises)
-                    if(res) result.success(res)
-                    else result.error("[my_target_flutter]", " can not initialise", null)
-                }
+                initAd(context, data.slotId, data.useDebugMode ?: false, data.testDevises, result)
+
             }
             LOAD -> load(result)
             SHOW -> show(result)
@@ -39,38 +39,49 @@ class MethodCallHandler(private val context: Context, private val adListenerChan
 
     }
 
-    private fun initAd(context: Context, slotId: Int, useDebugMode: Boolean, testDevises: String?): Boolean {
-       try {
-           MyTargetManager.initSdk( context)
-           MyTargetManager.setDebugMode(useDebugMode)
-           val configBuilder = MyTargetConfig.Builder().withTestDevices(testDevises).build()
-           MyTargetManager.setSdkConfig(configBuilder)
-           ad = InterstitialAd(slotId, context)
-           val listener = AdListener(adListenerChannel)
-           ad.setListener(listener)
-       } catch (e: Exception) {
-           return false
-       }
-        isInitialised = true
-        return true
+    private fun initAd(
+        context: Context,
+        slotId: Int?,
+        useDebugMode: Boolean,
+        testDevises: String?,
+        result: MethodChannel.Result,
+    ) {
+        if (slotId == null) {
+            result.error(errorCode, " slotId cant be null", null)
+        } else {
+            try {
+                MyTargetManager.initSdk(context)
+                MyTargetManager.setDebugMode(useDebugMode)
+                val configBuilder = MyTargetConfig.Builder().withTestDevices(testDevises).build()
+                MyTargetManager.setSdkConfig(configBuilder)
+                ad = InterstitialAd(slotId, context)
+                val listener = AdListener(adListenerChannel)
+                ad.setListener(listener)
+                isInitialised = true
+                result.success(true)
+            } catch (e: Exception) {
+                result.error(errorCode, " can not initialise", null)
+            }
+        }
+
     }
 
     private fun load(result: MethodChannel.Result) {
-        if(isInitialised) {
+        if (isInitialised) {
             ad.load()
 
             result.success(true)
         } else {
-            result.error("Ad not initialised", null, null)
+            result.error(errorCode, "Ad not initialised", null)
         }
     }
 
     private fun show(result: MethodChannel.Result) {
-        if(isInitialised) {
+        if (isInitialised) {
             ad.show()
             result.success(true)
         } else {
-            result.error("Ad not initialised", null, null)
+            result.error(errorCode,"Ad not initialised", null)
         }
 
     }
